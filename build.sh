ab5929bd0f2d486afdc1b0deb6a64a862dc381d8
Vincent <prevosto@mit.edu>
2023-01-02 23:55:40 -0500

    /root doesn't work with Singularity

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t wanglabneuro/whisk:latest -t wanglabneuro/whisk:0.0.1 -f Dockerfile context
+docker build -t wanglabneuro/whisk:latest -t wanglabneuro/whisk:0.0.2 -f Dockerfile context
